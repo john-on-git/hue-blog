a241fd90a11f8b307321a7dbf72424173a97a2a9
John Gregg-Webster <john.gregg-webster.2021@uni.strath.ac.uk>
2024-09-24 00:00:46 +0100

    basic implementation w/ static color

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import './App.css';
+import React from 'react';
+import headerImage from './header.png';
+import CONFIG from './config';
+import BLOG_POSTS from './blogPosts';
+
+
+
+class App extends React.Component {
+	componentDidMount(): void {
+		document.title = CONFIG.BLOG_NAME;
+	}
+	render(): React.JSX.Element {
+		return (
+		  <div className="App">
+				<div id="main-bar" className="color-dark">
+				  <header>
+					  <img id="header-image" src={headerImage}></img>
+					  <h1 id="header-text">{CONFIG.BLOG_NAME}</h1>
+				  </header>
+				  <div id="blog-info" className="flex-horizontal">
+					  <div id="about-container" className="color-light">
+						  <h2 id="about-title">About</h2>
+						  <p id="about-body">{CONFIG.ABOUT}</p>
+					  </div>
+					  <div id="contacts-list" className="color-light">
+						<h2>Contact</h2>
+						{CONFIG.CONTACT.map((row: {method:string, value:string}) => {
+							//special handling for clickable methods
+							let contactValue: React.JSX.Element;
+							switch(row.method) {
+								case "email":
+									contactValue = <a className="contact-value" href={`mailto:${row.value}`}>{row.value}</a>;
+									break;
+								default:
+									contactValue = <p className="contact-value">{row.value}</p>;
+								break;
+							}
+
+							return <div className="contact-row color-light" key={row.method}>
+								<p className="contact-method">{row.method}</p>
+								{contactValue}
+							</div>
+						})}
+					  </div>
+				  </div>
+				  <div id="posts-list">
+					{BLOG_POSTS.map((post: {key:number, title:string, content:string}) => {
+						return <div className="post color-light" key={post.key}>
+							<h2 className="post-title">{post.title}</h2>
+							<p className="post-content">{post.content}</p>
+						</div>
+					})}
+				  </div>
+			  	</div>
+		  </div>
+		);
+	  }
+}
+
+export default App;

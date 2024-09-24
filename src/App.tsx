92223f1e98cb61eed841c9672ec95cea9fb510cc
John Gregg-Webster <john.gregg-webster.2021@uni.strath.ac.uk>
2024-09-24 22:16:43 +0100

    set up blog to change color based on device time, emulating a day-night cycle.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const POSTS_PER_PAGE = 5;
 class App extends React.Component {
 	state = {
 		blogName: CONFIG.BLOG_NAME,
-		hue: CONFIG.HUE,
+		hue: this.GetTimeHue(),
 		about: CONFIG.ABOUT,
 		contactMethods: CONFIG.CONTACT,
 		allPosts: (()=> {
@@ -34,9 +34,59 @@ class App extends React.Component {
 		visiblePosts: [],
 	};
 
+	GetTimeHue()
+	{
+		function lerp(a:[number,number,number],b:[number,number,number], c:number): [number,number,number] {
+			return [
+				(a[0]*c) + (b[0]*(1-c)), 
+				(a[1]*c) + (b[1]*(1-c)), 
+				(a[2]*c) + (b[2]*(1-c))
+			];
+		}
+		const now = new Date();
+		const sunrise = 7;
+		const timeInSeconds = ((now.getHours()-sunrise) * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds();
+		
+		const timeInRadians = timeInSeconds / 86400 * (Math.PI*2);
+		const lerpPoint = (Math.sin(timeInRadians)+1)/2;
+		
+		const midday:[number,number,number]    = [255,193,  0];
+		const midnight: [number,number,number] = [ 10, 18, 94];
+		let rgbColor = lerp(midday,midnight,lerpPoint);
+		//RGB -> HSL formula from Wikipedia
+		//this is the broken part
+		rgbColor = [rgbColor[0]/255, rgbColor[1]/255, rgbColor[2]/255] //r/g/b must be in range 0-1
+		const min = Math.min(rgbColor[0], rgbColor[1], rgbColor[2]); 
+		if(rgbColor[0]>rgbColor[1] && rgbColor[0]>rgbColor[2]) { //max is red
+			const v = rgbColor[0];
+			const l = (min + v)/2;
+			const c = 2 * (v-l);
+			return 60 * (((rgbColor[1]-rgbColor[2])/c)%6); //g-b
+		}
+		else if(rgbColor[1]>rgbColor[0] && rgbColor[1]>rgbColor[2]) { //max is green
+			const v = rgbColor[1];
+			const l = (min + v)/2;
+			const c = 2 * (v-l);
+			return 60 * (((rgbColor[2]-rgbColor[0])/c)+2); //b-r
+		}
+		else { //max is blue
+			const v = rgbColor[2];
+			const l = (min + v)/2;
+			const c = 2 * (v-l);
+			return 60 * (((rgbColor[0]-rgbColor[1])/c)+4); //r-g
+		}
+	}
+
 	componentDidMount(): void {
 		document.title = CONFIG.BLOG_NAME;
 		this.fetchPosts(POSTS_PER_PAGE);
+		//set the hue based on current time, emulating a day-night cycle
+		window.setInterval(
+			() => this.setState({
+				hue:this.GetTimeHue(),
+			}),
+			1000
+		)
 	}
 		
 	fetchPosts(n:number) {
